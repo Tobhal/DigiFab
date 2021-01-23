645eb622d6a8099c953bc8e86b51708c4d99b087
Tobias Hallingstda <tobias@hallingstad.me>
2021-01-23 17:24:12 +0100

    Satt opp selve siden fra en eldre side jeg har

---
+++ docs/scripts/script.js
@@ -0,0 +1,156 @@
+window.onload = startup;
+
+let hideDataThing = [
+  "about",
+  "exam",
+  "task1",
+  "task2",
+//  "tast3",
+//  "tast4",
+//  "tast5",
+//  "tast6",
+//  "tast7",
+//  "tast8",
+//  "tast9",
+//  "tast10"
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  //console.log(userPrefersDark ? "dark mode thing":"light mode");
+
+  if (localStorage.getItem("dark") == null) {
+    localStorage.setItem("dark", "false");
+  } else if (localStorage.getItem("dark") == "true") {
+    document.body.style.backgroundColor == "rgba(28, 28, 30, 1)";
+  };
+
+  if (userPrefersDark) {
+    localStorage.setItem("dark", "true")
+  }
+
+
+  // Hide the [...] on load
+  for (element of hideDataThing) {
+    document.getElementById(element + "Collaps").style.display = "none";
+  }
+
+  (function () {
+    changeCodeFontSize(0)
+    codeView()
+    }
+  )()
+
+
+});
+
+function startup() {
+
+  if (localStorage.getItem("dark") == "true") {
+    localStorage.setItem("dark", "true");
+    document.documentElement.classList.toggle('theme--dark');
+    document.getElementById('theme').classList.toggle('c-toggle--active');
+  }
+  
+  document.body.classList.add('notransition');
+  
+  // Setts upp the ability to hide a header
+  document.addEventListener("click", (event) => {
+    for (el of hideDataThing) {
+      if (event.target.matches("#" + el)) {
+        hideData(el);
+      }
+    }
+  });
+
+  document.getElementById("themeButton").addEventListener("click", () => {
+    darkmode();
+  });
+
+  let element = document.getElementsByClassName("date")
+  for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", () => {
+      changeDateInfo(element, i);
+    });
+    localStorage.setItem("date" + i, "true");
+  };
+
+  element = document.getElementsByClassName("course")
+  for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", () => {
+      hideCourseElement(element, i)
+    })
+  }
+
+};
+
+feather.replace();
+
+function hideData(data) {
+
+  let element = document.getElementsByClassName(data)[0];
+  let elementCollaps = data + "Collaps";
+
+  if (element.style.display != "none") {
+    // Hide
+    for (let i = 0; i < document.getElementsByClassName(data).length; i++) {
+      document.getElementsByClassName(data)[i].style.display = "none"
+    };
+    document.getElementById(elementCollaps).style.display = "";
+  } else {
+    // Show
+    for (let i = 0; i < document.getElementsByClassName(data).length; i++) {
+      document.getElementsByClassName(data)[i].style.display = ""
+    };
+    document.getElementById(elementCollaps).style.display = "none";
+  };
+};
+
+function hideCourseElement(element, index) {
+
+  let tempElement = element[index].childNodes[1]
+
+  tempElement.style.display = tempElement.style.display === 'block' ? 'none' : 'block';
+  
+}
+
+function darkmode(darkmode) {   // Change the site to dark mode or not. you can define a true (dark mode) of false (white mode), if not the it will just change color.
+  if (darkmode == null) {   // checks if there is a defined true/false. If not then flip the color.
+    localStorage.getItem("dark") == "true" ? localStorage.setItem("dark", "false") : localStorage.setItem("dark", "true");
+    document.documentElement.classList.toggle('theme--dark');
+    document.getElementById('theme').classList.toggle('c-toggle--active');
+
+  } else if (darkmode == true && localStorage.getItem("dark") == "false") {
+    localStorage.setItem("dark", "true");
+    document.documentElement.classList.toggle('theme--dark');
+    document.getElementById('theme').classList.toggle('c-toggle--active');
+
+  } else if (darkmode == false && localStorage.getItem("dark") == "true") {
+    localStorage.setItem("dark", "false");
+    document.documentElement.classList.toggle('theme--dark');
+    document.getElementById('theme').classList.toggle('c-toggle--active');
+  };
+};
+
+/* Code thing */
+function codeView () {
+  var pre = document.getElementsByTagName('code')
+  for (var i = 0; i < pre.length; i++) {
+      pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>'
+      var num = pre[i].innerHTML.split(/\n/).length
+      for (var j = 0; j < num; j++) {
+          var lineNum = pre[i].getElementsByTagName('span')[0]
+          lineNum.innerHTML += '<span>' + (j + 1) + '</span>'
+      }
+  }
+}
+
+function changeCodeFontSize (size) {
+  if (size == 0) { return }
+  var elements = document.getElementsByTagName('code')
+  for (var i = 0; i < elements.length; i++) {
+      elements[i].style['font-size'] = size + 'px'
+  }
+}
+/* Code end */
